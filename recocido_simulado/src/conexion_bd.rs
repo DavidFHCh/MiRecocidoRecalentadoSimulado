00260c497a4cd503c1b8dd86a1e2dfffdbc998c5
DavidFHCh <davfep_96@ciencias.unam.mx>
2017-09-08 19:46:15 -0500

    Se termina el manejo de la base de datos.

--- recocido_simulado/src/conexion_bd.rs
+++ recocido_simulado/src/conexion_bd.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 use structs::ciudad::Ciudad;
 use structs::conexion::Conexion;
 
-const NUM_CIUDADES: usize = 1097;
+const NUM_CIUDADES: usize = 1098;
 
 /// Regresa un Result que contiene un vector de ciudades o un error de rusqlite.
 ///
@@ -15,7 +15,7 @@ const NUM_CIUDADES: usize = 1097;
 /// use recocido_simulado::conexion_bd::get_ciudades;
 /// fn main() {
 ///     let ciudades = get_ciudades().unwrap();
-///     assert_eq!(ciudades.len(),1098);
+///     assert_eq!(ciudades.len(),1099);
 /// }
 /// ```
 pub fn get_ciudades() -> Result<Vec<Ciudad>, rusqlite::Error> {
@@ -25,6 +25,20 @@ pub fn get_ciudades() -> Result<Vec<Ciudad>, rusqlite::Error> {
 
     let mut consulta = conexion.prepare("SELECT * FROM cities;").expect("NO SE PUDO COMPLETAR LA CONEXION.");
 
+    let mut consulta2 = conexion.prepare("SELECT * FROM connections").expect("NO SE PUDO COMPLETAR LA CONEXION 2.");
+
+    ciudades.push(
+        Ciudad {
+            ciudad_id: 0,
+            ciudad_nom: "Aldeeran".to_string(),
+            pais: "Desconocido".to_string(),
+            poblacion: 0,
+            latitud: 0.0,
+            longitud: 0.0,
+            vecinos: HashMap::new(),
+        }
+    );
+
     let c_it = consulta.query_map(&[], |renglon| {
         Ciudad {
             ciudad_id: renglon.get(0),
@@ -33,18 +47,32 @@ pub fn get_ciudades() -> Result<Vec<Ciudad>, rusqlite::Error> {
             poblacion: renglon.get(3),
             latitud: renglon.get(4),
             longitud: renglon.get(5),
+            vecinos: HashMap::new(),
         }
     }).unwrap();
 
-    
+    let con_it = consulta2.query_map(&[], |renglon| {
+        Conexion{
+            ciudad1: renglon.get(0),
+            ciudad2: renglon.get(1),
+            distancia: renglon.get(2),
+        }
+    }).unwrap();
 
     for ciudad in c_it {
         let ciudad_1 = ciudad.unwrap();
         ciudades.push(ciudad_1);
     }
 
+    for arista in con_it {
+        let aris = arista.unwrap();
+        let id1 = aris.ciudad1;
+        let id2 = aris.ciudad2;
+        let dis = aris.distancia;
+        ciudades[id1 as usize].vecinos.insert(id2,dis);
+        ciudades[id2 as usize].vecinos.insert(id1,dis);
+    }
+
     Ok(ciudades)
 
 }
-
-//read this for reference http://blackbeam.org/doc/mysql/index.html
--- recocido_simulado/src/structs/ciudad.rs
+++ recocido_simulado/src/structs/ciudad.rs
@@ -1,10 +1,21 @@
+
+use std::collections::HashMap;
+use std::fmt;
+
 #[derive(Clone)]
 pub struct Ciudad {
-    pub ciudad_id: i64,
+    pub ciudad_id: i32,
     pub ciudad_nom: String,
     pub pais: String,
     pub poblacion: i64,
     pub latitud: f64,
     pub longitud: f64,
-    pub vecinos: 
+    pub vecinos: HashMap<i32,f64>,
+}
+
+
+impl fmt::Debug for Ciudad {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Ciudad{{ id: {}, nombre: {}, pais: {} }}",self.ciudad_id, self.ciudad_nom,self.pais)
+    }
 }
